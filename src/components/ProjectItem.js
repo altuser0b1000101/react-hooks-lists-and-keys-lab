8623de391a5e23c8bfb0bee7f9bdde1922cc51a2
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-06-11 00:25:58 -0400

    commit tree 1ad3fecab14886638bb060b4dfb3767c2ab8b5c8

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function ProjectItem({ name, about, technologies }) {
   return (
-    <div className="project-item">
+    <div className='project-item'>
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div className='technologies'>
+        <span> {technologies[0]}</span>
+        <span> {technologies[1]}</span>
+        <span> {technologies[2]}</span>
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,40 @@
-import React from "react";
-import ProjectItem from "./ProjectItem";
+import React from 'react';
+import ProjectItem from './ProjectItem';
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
-    <div id="projects">
+    <div id='projects'>
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id='project-list'>
+        {projects.map((project) => {
+          return (
+            <ProjectItem
+              key={project.id}
+              name={project.name}
+              about={project.about}
+              technologies={project.technologies}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
+// <ProjectItem
+// name={projects[0].name}
+// about={projects[0].about}
+// technologies={projects[0].technologies[(0, 1)]}
+// // technologies={projects[0].technologies[1]}
+// />
+// <ProjectItem
+// name={projects[1].name}
+// about={projects[1].about}
+// technologies={projects[1].technologies[1]}
+// />
+// <ProjectItem
+// name={projects[2].name}
+// about={projects[2].about}
+// technologies={projects[2].technologies[2]}
